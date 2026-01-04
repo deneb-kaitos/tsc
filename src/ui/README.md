fbd1809ab4baa58ad8b3d2ec4b69bcec6277f1dc
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2026-01-04 23:41:44 +0100

    wip: UI

---
+++ src/ui/README.md
@@ -0,0 +1,20 @@
+# UI
+
+## zfs dataset
+
+```sh
+```
+doas zfs create \
+  -o mountpoint=/tank/projects/coast/svc/ui \
+  -o recordsize=16K \
+  -o compression=zstd \
+  -o atime=off \
+  -o xattr=sa \
+  -o acltype=posix \
+  tank/projects/coast/svc/ui
+```
+
+```
+## deployment
+
+TBD
---
+++ src/ui/deployment/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+SVC_NAME="redis"
+HOST_SRC_POINT="./host/"
+HOST_DST_POINT="/"
+JAIL_SRC_POINT="./jail/"
+JAIL_DST_POINT="/tank/projects/coast/svc/${SVC_NAME}/"
+
+echo "deploying to host"
+doas rsync -r --itemize-changes ${HOST_SRC_POINT} ${HOST_DST_POINT}
+echo "deploying to jail"
+doas rsync -r --itemize-changes ${JAIL_SRC_POINT} ${JAIL_DST_POINT}
+echo ""
